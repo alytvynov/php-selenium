d673f3ed1429fde7fcef2abd935dbf562f4a9a70
Anton Lytvynov <alytvynov@wynd.eu>
2020-04-19 21:26:48 +0300

    Clear

--- run.php
+++ run.php
@@ -4,23 +4,15 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
-use Facebook\WebDriver\WebDriverBy;
 
 $url = 'https://google.com';
-
 $host = 'http://localhost:4444';
+
 $driver = RemoteWebDriver::create($host, DesiredCapabilities::chrome());
 $driver->get($url);
-echo "The title is '" . $driver->getTitle() . "'\n";
-$driver->wait(30);
-$itemsSpan = $driver->findElement(
-    WebDriverBy::cssSelector('title')
-);
-$text = $itemsSpan->getText();
 
+echo sprintf("The title is '%s'\n", $driver->getTitle());
 echo $driver->getPageSource();
-
-shell_exec(sprintf('echo "%s" >> ./%s.html', $driver->getPageSource(), time()));
 
 $driver->quit();
 
